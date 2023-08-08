0a438c9f0c0f58844458539455a4f169d9d8990f
Kaya Arai-Yokoi <kaya.araiyokoi@gmail.com>
2023-08-08 11:16:46 +0200

    Create new game() function and modiified playRound() function.
    The playRound() function was modified so that each time a winner is
    determined, the result will be displayed on console via console.log
    and not via return.
     # Please enter the commit message for your
    changes. Lines starting

--- js/script.js
+++ js/script.js
@@ -11,32 +11,69 @@ function getComputerChoice() {
     }
 }
 
-getComputerChoice();
-
-function playRound(playerChoice, computerChoice) {
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = prompt("What is your choice?").toLowerCase();
     if (playerChoice == computerChoice) {
-        return "Tie"
+       console.log( "Tie");
+       return
     }
     else if (playerChoice == "rock" && computerChoice == "paper") {
-        return "You lose. Paper beats rock";
+        computerScore++;
+        console.log("You lose. Paper beats rock");
+        return
     }
     else if (playerChoice == "rock" && computerChoice == "scissors") {
-        return "You win. Rock beats scissors";
+        playerScore++;
+        console.log("You win. Rock beats scissors");
+        return
     }
     else if (playerChoice == "paper" && computerChoice == "scissors") {
-        return "You lose. Scissors beats paper";
+        computerScore++;
+        console.log("You lose. Scissors beats paper");
+        return
     }
     else if (playerChoice == "paper" && computerChoice == "rock") {
-        return "You win. Paper beats rock";
+        playerScore++;
+        console.log("You win. Paper beats rock");
+        return
     }
     else if (playerChoice == "scissors" && computerChoice == "rock") {
-        return "You lose. Rock beats scissors";
+        computerScore++;
+        console.log("You lose. Rock beats scissors");
+        return
     }
     else if (playerChoice == "scissors" && computerChoice == "paper") {
-        return "You win. Scissors beats paper";
+        playerScore++;
+        console.log("You win. Scissors beats paper");
+        return
+    }
+    else {
+        console.log("Please input a valid choice");
+        return
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 1; i <= 5; i++) {
+        console.log('Round $', i);
+        playRound();
+    }
+    if (playerScore > computerScore) {
+        return `Player wins with a score of ${playerScore}`;
+    } 
+    else if (playerScore < computerScore) {
+        return `Computer wins with a score of ${computerScore}`;
+    }
+    else {
+        return "Tie";
+    }
+    
+}
+
+let playerScore = 0;
+let computerScore = 0;
+console.log(playRound());
